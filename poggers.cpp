8d15e901879f761088c87d149df4f323214f19c6
Jake Mullins <jakemullins0@outlook.com>
2022-07-24 12:44:53 -0600

    Basically at 1-D Perlin Noise

---
+++ poggers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main (int argc, char* argv[]) { 
+
+    std::cout << argv[0] << " " << argv[1] << " " << argv[2] << std::endl;
+
+    int integer = 0;
+    integer += 5;
+
+    std::cout << "Poggers" << std::endl;
+}
