1d86048587aa7137c788416172749bd940b0fb2e
Jake Mullins <jakemullins0@outlook.com>
2022-07-26 20:49:19 -0600

    Can't get libnoise to include, copying files directly into working dir

---
+++ noise-test.cpp
@@ -0,0 +1,13 @@
+#ifndef NOISE_TEST_CPP
+#define NOISE_TEST_CPP
+
+#include <iostream> 
+#include <noise>
+
+int main (int argc, char** argv)
+{
+    noise::module::Perlin myPerlin;
+    std::cout << "What" << std::endl;
+}
+
+#endif
