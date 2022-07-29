6399634c5b087831a4dab602217050dcce8f376e
Jake Mullins <jakemullins0@outlook.com>
2022-07-28 22:34:44 -0600

    Transitioned to Ubuntu

--- noise-test.cpp
+++ noise-test.cpp
@@ -1,13 +1,17 @@
 #ifndef NOISE_TEST_CPP
 #define NOISE_TEST_CPP
 
-#include <iostream> 
-#include <noise>
+#include <iostream>
+#include <noise/noise.h>
+
+using namespace noise;
 
 int main (int argc, char** argv)
 {
-    noise::module::Perlin myPerlin;
-    std::cout << "What" << std::endl;
+    module::Perlin myModule;
+    double value = myModule.GetValue (1.25, 0.75, 0.50);
+    std::cout << value << std::endl;
+    return 0;
 }
 
 #endif
